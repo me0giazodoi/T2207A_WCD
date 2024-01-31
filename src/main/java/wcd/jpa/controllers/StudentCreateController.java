9170d32642b97c90c5f31f707ced5de24d06745e
me0giazodoi <hiepga1243@gmail.com>
2024-01-31 14:57:03 +0700

    create student (chon lop va mon hoc)

--- src/main/java/wcd/jpa/controllers/StudentCreateController.java
+++ src/main/java/wcd/jpa/controllers/StudentCreateController.java
@@ -8,9 +8,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import wcd.jpa.entities.Classes;
 import wcd.jpa.entities.Student;
+import wcd.jpa.entities.Subject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @WebServlet(value = "/create-student")
 public class StudentCreateController extends HttpServlet {
     private SessionFactory sessionFactory;
@@ -26,6 +34,17 @@ public class StudentCreateController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Classes> classesList = new ArrayList<>();
+        List<Subject> subjectList = new ArrayList<>();
+        // Query db
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            classesList = session.createQuery("FROM Classes ", Classes.class).getResultList();
+            subjectList = session.createQuery("FROM Subject ", Subject.class).getResultList();
+            session.getTransaction().commit();
+        }
+        req.setAttribute("classes",classesList);
+        req.setAttribute("subjectList",subjectList);
         req.getRequestDispatcher("student/createStudent.jsp").forward(req,resp);
     }
 
@@ -37,6 +56,19 @@ public class StudentCreateController extends HttpServlet {
         student.setAddress(req.getParameter("address"));
         try(Session session = sessionFactory.openSession()){
             session.beginTransaction();
+            // find Classes
+            int classID = Integer.parseInt(req.getParameter("class_id"));
+            Classes c = session.find(Classes.class,classID);
+
+            // find subjects
+            List<Integer> s_ids = Arrays.stream(req.getParameterValues("subject_id[]"))
+                    .map(Integer::parseInt).collect(Collectors.toList());
+            Query<Subject> query = session.createQuery("FROM Subject WHERE id IN (:ids)", Subject.class);
+            query.setParameter("ids", s_ids);
+            List<Subject> subjects = query.getResultList();
+            student.setSubjects(subjects);
+
+
             session.save(student);
             session.getTransaction().commit();
         }
--- src/main/webapp/student/createStudent.jsp
+++ src/main/webapp/student/createStudent.jsp
@@ -1,3 +1,6 @@
+<%@ page import="wcd.jpa.entities.Classes" %>
+<%@ page import="java.util.List" %>
+<%@ page import="wcd.jpa.entities.Subject" %><%--
 <%--
   Created by IntelliJ IDEA.
   User: ADMIN
@@ -27,8 +30,28 @@
                 <label for="address" class="form-label">Address</label>
                 <input type="text" name="address" class="form-control" id="address">
             </div>
+            <div class="mb-3">
+                <label for="class" class="form-label">Class</label>
+                <select name="class_id" class="form-control" id="class">
+                    <% for (Classes c: (List<Classes>)request.getAttribute("classesList")){ %>
+                    <option value="<%= c.getId()%>"><%= c.getName() %></option>
+                    <% } %>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="subject" class="form-label">Subject</label>
+                <% for (Subject s: (List<Subject>)request.getAttribute("subjectList")){ %>
+                <div class="form-check">
+                    <label class="form-check-label">
+                        <input name="subject_id[]" class="form-check-input" type="checkbox" value="<%= s.getId() %>" id="subject">
+                        <%= s.getName() %>
+                    </label>
+                </div>
+                <% } %>
+            </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
 </html>
+// Chuyen mang các giá tri chọn nhieu gia tri
